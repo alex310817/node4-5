b02ef93e910bc2b3dce0ce982db74a78a903752b
alex <alex310817@gmail.com>
2024-06-03 11:37:37 +0300

    роутер диш

---
+++ routes/dish.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+// Controllers
+const {
+  getAllDishes,
+  getDishById,
+  createDish,
+  updateDish,
+  deleteDish
+} = require('../controllers/dishController');
+
+// Routes
+router.get('/', getAllDishes);
+router.get('/:dishId', getDishById);
+router.post('/', createDish);
+router.put('/:dishId', updateDish);
+router.delete('/:dishId', deleteDish);
+
+module.exports = router;
