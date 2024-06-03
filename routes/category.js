817b74498c8deedfa974fe2d71454782c9af1aae
alex <alex310817@gmail.com>
2024-06-03 11:36:23 +0300

    роутер категория

---
+++ routes/category.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+// Controllers
+const {
+  getAllCategories,
+  getCategoryById,
+  getDishesByCategoryId,
+  createCategory,
+  updateCategory,
+  deleteCategory
+} = require('../controllers/categoryController');
+
+// Routes
+router.get('/', getAllCategories);
+router.get('/:categoryId', getCategoryById);
+router.get('/:categoryId/dishes', getDishesByCategoryId);
+router.post('/', createCategory);
+router.put('/:categoryId', updateCategory);
+router.delete('/:categoryId', deleteCategory);
+
+module.exports = router;
